62a8fa5bf1903424ce68fedce2a8b0d6f943b8b3
Nick Butler <nwmbutler@gmail.com>
2020-09-11 15:31:49 +0100

    practising inline styling, button

--- src/App.js
+++ src/App.js
@@ -28,14 +28,23 @@ class App extends Component {
         { name: event.target.value, age: 37 }
         ] 
       })
-
   }
 
   render() {
+    const style = {
+      backgroundColour: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
-        <button onClick={() => this.switchNameHandler("Nicholas")}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("Nicholas")}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
